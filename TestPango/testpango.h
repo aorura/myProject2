c33898fa15e2074fdf7d5448e3e0ea845c768509
박성철/주임연구원/AVN개발6팀 2파트(chalse.park@lge.com) <chalse.park@lge.com>
2016-02-15 17:48:16 +0900

    add speller

--- TestPango/testpango.h
+++ TestPango/testpango.h
@@ -21,15 +21,72 @@ private:
 
 	void init_font_family();
 	void init_pango_enums();
+	void onApplyAttributes();
+	void onApplSpeller();
 
 private slots:
-	void onApplyAttributes();
+	void onApply();
+//	void onApplyAttributes();
 	void onOpenUTF8File();
+	void appendText1();
+	void appendText2();
+	void appendText3();
+	void appendText4();
+	void appendText5();
+	void appendText6();
+	void appendText7();
+	void appendText8();
+	void appendText9();
+	void appendText10();
+	void appendText11();
+	void appendText12();
+	void appendText13();
+	void appendText14();
+	void appendText15();
+	void appendText16();
+	void appendText17();
+	void appendText18();
+	void appendText19();
+	void appendText20();
+	void appendText21();
+	void appendText22();
+	void appendText23();
+	void appendText24();
+	void appendText25();
+	void appendText26();
+	void appendText27();
+	void appendText28();
+	void appendText29();
+	void appendText30();
+	void appendText31();
+	void appendText32();
+	void appendText33();
+	void appendText34();
+	void appendText35();
+	void appendText36();
+	void appendText37();
+	void appendText38();
+	void appendText39();
+	void appendText40();
+	void appendText41();
+	void appendText42();
+	void appendText43();
+	void appendText44();
+	void appendText45();
+	void appendText46();
+	void appendText47();
+	void appendText48();
+	void appendText49();
+	void appendText50();
+	void appendText51();
 
 private:
 	Ui::TestPangoClass ui;
 
 	QImage m_image;
+	QString str;
+	int idx;
+	bool isSpellerEnter;
 };
 
 #endif // TESTPANGO_H
